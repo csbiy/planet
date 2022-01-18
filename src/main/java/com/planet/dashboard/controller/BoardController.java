112a4805c289c2829a7d768a194573260cc418d9
devcs96 <katd6@naver.com>
2022-01-18 16:17:56 +0900

    style($templates/fragments) : free UI template added and refactorying using template layout function of thymeleaf
    free ui template can be found here : https://startbootstrap.com/theme/sb-admin-2 (from bootstrap)

--- src/main/java/com/planet/dashboard/controller/BoardController.java
+++ src/main/java/com/planet/dashboard/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,7 +18,7 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping
-    public String getBoardList(Integer pageNum , Model model ){
+    public String getBoardList(@RequestParam Integer pageNum , Model model ){
         model.addAttribute("boards",boardService.getBoardsSortedByrCreatedAt(pageNum));
         return "board";
     }
